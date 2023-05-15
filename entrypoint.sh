acab04c1fbfc0df3988ed9a07cff8b7645ec9a9d
vlkjildi001 <106436614+vlkjildi001@users.noreply.github.com>
2023-05-15 01:48:10 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ WSPATH=${WSPATH:-'argo'}
 UUID=${UUID:-'c664751b-b662-48f9-b2aa-2f0d4d337a7f'}
 
 # 其他Paas保活
-PAAS1_URL=
+PAAS1_URL=https://render-0515.onrender.com/
 PAAS2_URL=
 PAAS3_URL=
 PAAS4_URL=
